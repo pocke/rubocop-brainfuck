bd8b0dd3a133f43efdcf0e59004b6ec33151c181
Masataka Kuwabara <kuwabara@pocke.me>
2017-03-18 16:23:39 +0900

    Override MAX_ITERATIONS

--- lib/rubocop/brainfuck.rb
+++ lib/rubocop/brainfuck.rb
@@ -1,6 +1,8 @@
 require 'rubocop'
 
 module RuboCop
+  Runner::MAX_ITERATIONS = Float::INFINITY
+
   module Brainfuck
     PROJECT_ROOT   = Pathname.new(__dir__).parent.parent.expand_path.freeze
     CONFIG_DEFAULT = PROJECT_ROOT.join('config', 'default.yml').freeze
